74b80c027e027f2d4d1ffe29cec4d45c058ef08e
Zekiah-A <zekiahamoako@outlook.com>
2021-12-27 01:50:57 +0000

    Profile Template prototype improvements

--- Grids.Avalonia/Models/Profile.cs
+++ Grids.Avalonia/Models/Profile.cs
@@ -8,30 +8,61 @@ namespace Grids.Avalonia.Models;
 
 public class Profile
 {
-    public IImage ProfileImage { get;  }
+    public IImage ProfileImage { get; }
     public string Username { get; }
     public string Email { get; }
-    
-    public string Number { get; }
-    public string RealName { get; }
-    public string Address { get; }
-    
-    public string LastSeen { get; }
-    public string Rank { get; }
-    public string Shared { get; }
-    
+    public string Description { get; }
 
-    public Profile (string profileImage, string username, string email, string number, string realName, string address, string lastSeen, string rank, string shared)
+    private string number;
+    public string Number
+    {
+        get => number;
+        set => number = "Number: " + value;
+    }
+    private string realName;
+    public string RealName
+    {
+        get => realName;
+        set => realName = "Real Name: " + value;
+    }
+    private string address;
+    public string Address
+    {
+        get => address;
+        set => address = "IP Address: " + value;
+    }
+    
+    private string lastSeen;
+    public string LastSeen
+    {
+        get => lastSeen;
+        set => lastSeen = "Last Message: " + value;
+    }
+    private string rank;
+    public string Rank
+    {
+        get => rank;
+        set => rank = "Server Rank: " + value;
+    }
+    private string mutual;
+    public string Mutual
+    {
+        get => mutual;
+        set => mutual = "Mutual Servers: " + value;
+    }
+    
+    public Profile (string profileImage, string username, string email, string description, string number, string realName, string address, string lastSeen, string rank, string mutual)
     {
         var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
         ProfileImage = new Bitmap(assets.Open(new Uri($"avares://Grids.Avalonia/{profileImage}")));
         Username = username;
         Email = email;
+        Description = description;
         Number = number;
         RealName = realName;
         Address = address;
         LastSeen = lastSeen;
         Rank = rank;
-        Shared = shared;
+        Mutual = mutual;
     }
 }
--- Grids.Avalonia/Views/MainWindow.axaml.cs
+++ Grids.Avalonia/Views/MainWindow.axaml.cs
@@ -57,21 +57,14 @@ public class MainWindow : ReactiveWindow<MainWindowViewModel>
         headerListBox!.SelectedIndex = -1;
     }
     
-    object profileContent = new Profile("../Assets/ProfileImages/z.png", "zekiahepic", "zekiahepic@irc.net", "00000000", "zekiahAmo", "73.02.134.5", "01/07/20201", "Operator", "#channel on server1");
-    
-    //public object ProfileContent
-    //{
-    //    get => profileContent;
-    //    set => this.RaiseAndSetIfChanged(ref profileContent, value);
-    //}
-
-
     private async Task ShowProfileDialogAsync(InteractionContext<MainWindowViewModel, ProfileWindowViewModel?> interaction)
     {
         var profileDialog = new ProfileWindow();
-        profileDialog.Content = profileContent;
         profileDialog.DataContext = interaction.Input;
         
+        var profileContent = new Profile("../Assets/ProfileImages/z.png", "zekiahepic", "zekiahepic@irc.net", "I am but a sad soul, just looking for deez nuts.", "00000000", "zekiahAmo", "73.02.134.5", "01/07/20201", "Operator", "#channel on server1");
+        profileDialog.Content = profileContent;
+
         var result = await profileDialog.ShowDialog<ProfileWindowViewModel?>(this);
         interaction.SetOutput(result);
     }
